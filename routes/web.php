47c64f2493a44ddd3a487f397216e0876e0c62ca
liuyu <liuyuit@aliyun.com>
2021-04-28 16:10:42 +0800

    feature: 建立 socket 连接

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    // 首页显示二维码
+    return view('hello');
+});
+
+Route::get('login', function () {
+    // 扫码后，手机显示的登陆页
+    return view('login');
+});
+
+Route::post('/login', function (\Illuminate\Http\Request $request) {
+    // 为了尽可能简化流程，我们就不执行数据库查询的逻辑了，直接在 session 中写入信息吧
+    session([
+        'login_info' => $request->all(),
+    ]);
+    // 返回响应
+    return response([
+        'code' => 0,
+        'message' => '登陆成功',
+        'data' => session('login_info'),
+    ]);
+});
